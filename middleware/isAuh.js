adcbbe4033308f89e28be847d0bf60cc52e9a017
sai sinare <saisinare19@gmail.com>
2024-01-16 18:29:37 +0530

    origin main

--- middleware/isAuh.js
+++ middleware/isAuh.js
@@ -5,8 +5,9 @@ const JWT_SECRETE = process.env.JWT_SCERETE;
 const fetchuser = (req,res,next)=>{
     const token = req.cookies.token
     console.log(req.cookies.token)
+    console.log(req.cookies)
     if(!token){
-        return res.status(401).json({message:"enter Valid Token",JWT:JWT_SECRETE,token:token})
+        return res.status(401).json({message:"enter Valid Token",JWT:JWT_SECRETE,token:token,cookies:req.cookies})
     }
     const data = jwt.verify(token,JWT_SECRETE)
     req.userId = data.id 
