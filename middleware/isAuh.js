79e2508b6da86faa0a9d3ce9981347d4f054ffc1
sai sinare <saisinare19@gmail.com>
2024-01-16 20:06:01 +0530

    cookieparser middleware extra added

--- middleware/isAuh.js
+++ middleware/isAuh.js
@@ -1,18 +1,23 @@
 const jwt = require("jsonwebtoken");
-const dotenv = require('dotenv')
-dotenv.config()
-const cookieParser = require("cookie-parser")
-cookieParser();
+const dotenv = require("dotenv");
+dotenv.config();
 const JWT_SECRETE = process.env.JWT_SCERETE;
-const fetchuser = (req,res,next)=>{
+const fetchuser = (req, res, next) => {
+  cookieParser();
+  const token = req.cookies.token;
+  if (!token) {
+    return res
+      .status(401)
+      .json({
+        message: "enter Valid Token",
+        JWT: JWT_SECRETE,
+        token: token,
+        cookies: req.cookies,
+      });
+  }
+  const data = jwt.verify(token, JWT_SECRETE);
+  req.userId = data.id;
+  next();
+};
 
-    const token = req.cookies.token
-    if(!token){
-        return res.status(401).json({message:"enter Valid Token",JWT:JWT_SECRETE,token:token,cookies:req.cookies})
-    }
-    const data = jwt.verify(token,JWT_SECRETE)
-    req.userId = data.id 
-    next()
-}
-
-module.exports = fetchuser
+module.exports = fetchuser;
--- routes/user/products.js
+++ routes/user/products.js
@@ -2,11 +2,12 @@ const express = require("express");
 const Router = express.Router();
 const productController = require("../../controller/user/productController");
 const isAuth = require("../../middleware/isAuh");
+const cookieParser = require("cookie-parser");
 Router.get("/products", productController.getProducts);
 Router.get("/checkout", isAuth, productController.getCheckout);
 Router.get("/product/:productId", productController.getProduct);
 Router.get("/cart/products", isAuth, productController.getCartProducts);
-Router.post("/cart/add/:productId", isAuth, productController.postAddtoCart);
+Router.post("/cart/add/:productId", cookieParser,isAuth, productController.postAddtoCart);
 Router.post("/order", isAuth, productController.postOrder);
 Router.get("/orders", isAuth, productController.getOrders);
 Router.delete("/api/cart/:productId", isAuth, productController.deleteFromCart);
@@ -20,5 +21,9 @@ Router.put(
 Router.get("/order/:productId", isAuth, productController.getOrder);
 Router.post("/review", isAuth, productController.postReview);
 Router.get("/review/:productId", productController.getReview);
-Router.get("/orders/invoice/:orderId",isAuth,productController.getOrderInvoice);
+Router.get(
+  "/orders/invoice/:orderId",
+  isAuth,
+  productController.getOrderInvoice
+);
 module.exports = Router;
